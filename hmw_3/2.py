35086a841a1972ce18a6480d9cce2ab686d79f27
Saint-Jacques <mishasanjak@gmail.com>
2021-10-04 19:39:02 +0300

    feat: impl first and second task

---
+++ hmw_3/2.py
@@ -0,0 +1,9 @@
+def format_user(
+        name="name",
+        last_name="",
+        year="",
+        city="",
+        email="",
+        phone_number=""
+    ):
+    print(f"{name} {last_name} {year} {city} {email} {phone_number}")
